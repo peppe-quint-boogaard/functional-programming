693aef048c4ce85a98b65b74131b82786b2737e1
peppe-quint-boogaard <peppequintb@gmail.com>
2018-10-31 13:12:46 +0100

    if-else statement checkValue doesn't work

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// var list = document.getElementById("boekenlijst");
 const OBA = require('oba-api');
 
 // Setup authentication to api server
@@ -8,38 +7,37 @@ const client = new OBA({
   secret: '4289fec4e962a33118340c888699438d'
 });
 
-// General usage:
-// client.get({ENDPOINT}, {PARAMS});
-// ENDPOINT = search | details | refine | schema | availability | holdings
-// PARAMS = API url parameter options (see api docs for more info)
+function checkValue(author) {
+  console.log(author);
+  if (typeof author === "undefined") {
+    // console.log('Het is undefined');
+    return 'Author unknown';
+  } else {
+    // console.log('Het loopt goed');
+    return author;
+  }
+}
 
-// Client returns a promise which resolves the APIs output in JSON
-
-// Example search to the word 'rijk' sorted by title:
 client.get('search', {
-  //  nederlandse wetenschappers in het buitenland biologie
-  //  zoekterm
-  //
-  q: 'nederlandse wetenschappers in het buitenland',
-  sort: 'title',
-  page: '1',
+  q: 'oorlog in het buitenland',
+  sort: 'relevance',
+  page: '9',
   facet: 'type(book)',
   refine: true
 })
 .then(function(results) {
-  const boeken = [];
-  JSON.parse(results).aquabrowser.results.result.forEach(function(boek, id) {
-    // console.log(boek.titles.title.$t, id); // TITEL VAN HET BOEK
-    // console.log(boek.authors.author.$t); // AUTEUR VAN HET BOEK
-    // console.log(boek.publication.year.$t); // JAAR VAN PUBLICATIE
-    boeken.push(boek.titles.title.$t, id);
-    // boeken.map(function (value, index) {
-    //   return index;
-    // })
+  const resList = [];
+  JSON.parse(results).aquabrowser.results.result.forEach(function(book) {
+    var bookList = {
+      Title : book.titles.title.$t,
+      Year : book.publication.year.$t,
+      Author : checkValue(book.authors.author.$t·)
+      // Language : book.languages.language
+    }
+    // console.log(book); // Fallback results
+    resList.push([bookList]);
   })
-  console.log(boeken);
-
+  console.log(resList);
+  checkValue();
 })
 .catch(err => console.log(err)) // Something went wrong in the request to the API
-
-// Bij sommige autheurs werkt het wel, andere weer niet want dan staat er nog 'main-author' waardoor er niet bij te komen is.
