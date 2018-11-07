a9a155189aae89bac7a6156e66dd2691fb9aa8fa
peppe-quint-boogaard <peppequintb@gmail.com>
2018-11-07 12:00:07 +0100

    added files

---
+++ test.js
@@ -0,0 +1,9 @@
+const margin = 60;
+const width = 1000 - 2 * margin;
+const height = 600 - 2 * margin;
+
+const svg = d3.select('svg');
+
+const chart = svg.append('g').attr('transform', `translate(${margin}, ${margin})`);
+
+const yScale = d3.scaleLinear().range([height:])
