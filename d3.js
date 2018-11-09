22d98b4ec828549be81ebd3a961e13e1f2a393be
peppe-quint-boogaard <peppequintb@gmail.com>
2018-11-09 13:12:27 +0100

    get length of different years

--- d3.js
+++ d3.js
@@ -1,8 +1,62 @@
+const sample = [
+      {
+        language: 'Rust',
+        value: 78.9,
+        color: '#000000'
+      },
+      {
+        language: 'Kotlin',
+        value: 75.1,
+        color: '#00a2ee'
+      },
+      {
+        language: 'Python',
+        value: 68.0,
+        color: '#fbcb39'
+      },
+      {
+        language: 'TypeScript',
+        value: 67.0,
+        color: '#007bc8'
+      },
+      {
+        language: 'Go',
+        value: 65.6,
+        color: '#65cedb'
+      },
+      {
+        language: 'Swift',
+        value: 65.1,
+        color: '#ff6e52'
+      },
+      {
+        language: 'JavaScript',
+        value: 61.9,
+        color: '#f9de3f'
+      },
+      {
+        language: 'C#',
+        value: 60.4,
+        color: '#5d2f8e'
+      },
+      {
+        language: 'F#',
+        value: 59.6,
+        color: '#008fc9'
+      },
+      {
+        language: 'Clojure',
+        value: 59.6,
+        color: '#507dca'
+      }
+    ];
+
 const margin = 60;
 const width = 1000 - 2 * margin;
 const height = 600 - 2 * margin;
 
 const svg = d3.select('svg');
+const rect = d3.select('rect');
 
 const chart = svg.append('g').attr('transform', `translate(${margin}, ${margin})`);
 
@@ -10,5 +64,39 @@ const yScale = d3.scaleLinear().range([height, 0]).domain([0, 100]);
 chart.append('g').call(d3.axisLeft(yScale));
 
 const xScale = d3.scaleBand().range([0, width]).domain(sample.map((s) => s.language)).padding(0.2);
-
 chart.append('g').attr('transform', `translate(0, ${height})`).call(d3.axisBottom(xScale));
+
+chart.selectAll()
+    .data(sample)
+    .enter()
+    .append('rect')
+    .attr('x', (s) => xScale(s.language))
+    .attr('y', (s) => yScale(s.value))
+    .attr('height', (s) => height - yScale(s.value))
+    .attr('width', xScale.bandwidth())
+    .on('mouseenter', function (actual, i) {
+        d3.select(this).transition().duration(300).attr('opacity', 0.5).attr('x', (a) => xScale(a.language) - 5).attr('width', xScale.bandwidth() + 10)
+    })
+    .on('mouseleave', function (actual, i) {
+        d3.select(this).transition().duration(300).attr('opacity', 1).attr('x', (a) => xScale(a.language)).attr('width', xScale.bandwidth())
+    })
+
+chart.append('g')
+    .attr('class', 'grid')
+    .call(d3.axisLeft()
+        .scale(yScale)
+        .tickSize(-width, 0, 0)
+        .tickFormat(''))
+
+svg.append('text')
+    .attr('x', -(height / 2) - margin)
+    .attr('y', margin / 2.4)
+    .attr('transform', 'rotate(-90)')
+    .attr('text-anchor', 'middle')
+    .text('Love meter (%)')
+
+svg.append('text')
+    .attr('x', width / 2 + margin)
+    .attr('y', 40)
+    .attr('text-anchor', 'middle')
+    .text('Most loved programming languages in 2018')
--- index.js
+++ index.js
@@ -117,10 +117,43 @@ Promise.all(responses).then(function(totalRes) {
   // sort results on the location of the publishers
   uniqueResults.sort((a, b) => (a.Year < b.Year ? -1 : a.Year > b.Year ? 1 : 0));
 
+  // array different years
+  let firstYear = []; // 1940
+  let secondYear = []; // 1941
+  let thirdYear = []; // 1942
+  let fourthYear = []; // 1943
+  let fifthYear = []; // 1944
+  let sixthYear = []; // 1945
+
+  // get books from years
+  uniqueResults.forEach(function(uniqueYear) {
+    if (uniqueYear.Year === '1940') {
+      firstYear.push(uniqueYear);
+    } else if (uniqueYear.Year === '1941') {
+      secondYear.push(uniqueYear);
+    } else if (uniqueYear.Year === '1942') {
+      thirdYear.push(uniqueYear);
+    } else if (uniqueYear.Year === '1943') {
+      fourthYear.push(uniqueYear);
+    } else if (uniqueYear.Year === '1944') {
+      fifthYear.push(uniqueYear);
+    } else if (uniqueYear.Year === '1945') {
+      sixthYear.push(uniqueYear);
+    }
+  });
+
+  // number of books per year
+  console.log(firstYear.length);
+  console.log(secondYear.length);
+  console.log(thirdYear.length);
+  console.log(fourthYear.length);
+  console.log(fifthYear.length);
+  console.log(sixthYear.length);
+
   // https://stackabuse.com/reading-and-writing-json-files-with-node-js/
   let data = JSON.stringify(uniqueResults, null, 2);
   fs.writeFileSync('oba-data.json', data, 'utf8');
 
-  console.log(uniqueResults);
-  console.log(uniqueResults.length); // total amount results
+  //console.log(uniqueResults);
+  // console.log(uniqueResults.length); // total amount results
 })
